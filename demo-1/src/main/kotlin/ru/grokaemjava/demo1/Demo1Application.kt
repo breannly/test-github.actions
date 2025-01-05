bbaefa5c111059b46b49287b32991d3461458104
armenkatvalyan <katvalyan03@bk.ru>
2025-01-05 03:28:38 +0300

    skadaskdksakdas

--- demo-1/src/main/kotlin/ru/grokaemjava/demo1/Demo1Application.kt
+++ demo-1/src/main/kotlin/ru/grokaemjava/demo1/Demo1Application.kt
@@ -6,6 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class Demo1Application
 
-fun main(args: Array<String>) {
-    runApplication<Demo1Application>(*args) // t
+fun main(vararg args: String) {
+    runApplication<Demo1Application>(*args)
 }
