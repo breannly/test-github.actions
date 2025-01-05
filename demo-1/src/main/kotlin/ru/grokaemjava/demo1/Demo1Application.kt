0b357a7d2372f646d2eabe6b06d48ffc454170e3
armenkatvalyan <katvalyan03@bk.ru>
2025-01-05 03:26:14 +0300

    fdsfdsjfjsfjsd

--- demo-1/src/main/kotlin/ru/grokaemjava/demo1/Demo1Application.kt
+++ demo-1/src/main/kotlin/ru/grokaemjava/demo1/Demo1Application.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class Demo1Application
 
 fun main(args: Array<String>) {
-    runApplication<Demo1Application>(*args)
+    runApplication<Demo1Application>(*args) // t
 }
